48faf0d6573d4b3962c2c8a9d8f5998c7b21db12
Irfan wani <irfanwani347@gmail.com>
2021-02-04 14:35:06 +0530

    completed my project

--- movieScreens/details.js
+++ movieScreens/details.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { ScrollView, Text, Image, Dimensions, StyleSheet } from 'react-native'
 
 export default class MoreDetails extends React.Component {
     constructor(props) {
@@ -27,12 +27,35 @@ export default class MoreDetails extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>This is details page and this is the id: {this.props.navigation.getParam('i')}</Text>
-                <Text>{this.state.details.Title}</Text>
-                <Text>{this.state.details.Year}</Text>
-                <Text>{this.state.details.Released}</Text>
-            </View>
+            <ScrollView>
+                <Image resizeMode='contain' style={{height: Dimensions.get('window').height/2, width: Dimensions.get('window').width}} source={{uri: this.state.details.Poster}} />                
+                <Text style={styles.text}>Title: {this.state.details.Title}</Text>
+                <Text style={styles.text}>Language: {this.state.details.Language}</Text>
+                {/* <Text style={styles.text}>Ratings: {this.state.details.Ratings[0].Value}</Text> */}
+                <Text style={styles.text}>Year: {this.state.details.Year}</Text>
+                <Text style={styles.text}>Released On: {this.state.details.Released}</Text>
+                <Text style={styles.text}>Rated: {this.state.details.Rated}</Text>
+                <Text style={styles.text}>Genre: {this.state.details.Genre}</Text>
+                <Text style={styles.text}>Director: {this.state.details.Director}</Text>
+                <Text style={styles.text}>Writer: {this.state.details.Writer}</Text>
+                <Text style={styles.text}>Actors: {this.state.details.Actors}</Text>
+                <Text style={styles.text}>Country: {this.state.details.Country}</Text>
+                <Text style={styles.text}>Awards: {this.state.details.Awards}</Text>
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 25,
+        marginHorizontal: 10,
+        fontWeight: 'normal',
+        marginVertical: 10,
+        borderWidth: 2,
+        borderColor: 'teal',
+        textAlign: 'center',
+        paddingHorizontal: 5,
+        borderRadius: 20
+    }
+})
--- movieScreens/search.js
+++ movieScreens/search.js
@@ -7,7 +7,7 @@ class Movie extends React.Component {
         return (
             <View style={styles.movie}>
                 <TouchableOpacity style={{flexDirection: 'row'}} onPress={() => this.props.navigation.navigate('secondStack', {i: this.props.movie.imdbID})}>
-                    <Image style={{height: 50, width: 50, marginLeft: 5}} source={{uri: this.props.movie.Poster}} />
+                    <Image resizeMode='contain' style={{height: 50, width: 50, marginLeft: 5}} source={{uri: this.props.movie.Poster}} />
                     <View>
                         <Text style={{marginLeft: 5, maxWidth: (Dimensions.get('window').width - 100)}}>{this.props.movie.Title}</Text>
                         <Text style={{marginLeft: 5}}>{this.props.movie.Year}</Text>
@@ -49,7 +49,7 @@ export default class Search extends React.Component {
                 <FlatList
                     ListHeaderComponent={() => <View style={styles.view}>
                                                 <Icon name='search' size={25} color='teal' />
-                                                <TextInput placeholder='Search here...' value={this.state.search} onChangeText={this.getSearch} />
+                                                <TextInput style={{paddingRight: Dimensions.get('window').width/2}} autoFocus={true} placeholder='Search here...' value={this.state.search} onChangeText={this.getSearch} />
                                             </View>} 
                     renderItem={this.renderItem} 
                     data={this.state.result} 
@@ -65,7 +65,6 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'teal',
         borderRadius: 20, 
-        paddingRight: 80,
         paddingVertical: 5,
         marginVertical: 10,
         marginHorizontal: 10
